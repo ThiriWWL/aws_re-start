52961043c7bcf72dd8309c6dbb6e27113664b65a
ThiriWWL <msthiriwharwharlwin@gmail.com>
2024-12-23 12:35:04 +0630

    first commit

---
+++ test_3.py
@@ -0,0 +1,13 @@
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+name = input("What is your name? Thiri")
+print(name)
+color = input("What is your favorite color? red ")
+animal = input("What is your favorite animal? tiger ")
+print("{}, you like a {} {}!".format(name,color,animal))
